a1be8f32be0d8fd5cf0c58c8978359fb35c66600
OceanGopher <epicdoggames@gmail.com>
2024-05-21 22:10:26 -0500

    Walls & Collisions To prevent the hero from going through gameObjects such as walls, we add grid positions that cannot be occupied by Person objects. We dynamically add and remove solid positions where the Person is so that we can overlap npcs.

--- Person.js
+++ Person.js
@@ -14,33 +14,49 @@ class Person extends GameObject {
     }
 
     update(state)  {
-        this.updatePosition();
-        this.updateSprite(state);
+        if (this.movingProgressRemaining > 0) {
+            this.updatePosition();
+        } else {
+            // more cases for starting to walk will come here
 
-        if (this.isPlayerControlled && this.movingProgressRemaining ===0 && state.arrow) {
-            // move to this direction
-            this.direction = state.arrow;
+            // case: we're keyboard ready and have an arrow pressed
+            if (this.isPlayerControlled && state.arrow) {
+                this.startBehavior(state, {
+                    type: "walk",
+                    direction: state.arrow
+                })
+            }
+            this.updateSprite(state);
+        }
+    }
+
+    startBehavior(state, behavior)  {
+        // set character direction to whatever behavior has
+        this.direction = behavior.direction;
+
+        if (behavior.type === "walk")  {
+            // stop here if space is not free
+            if (state.map.isSpaceTaken(this.x, this.y, this.direction))  {
+                return;
+            }
+            // ready to walk
+            state.map.moveWall(this.x, this.y, this.direction);
             this.movingProgressRemaining = 16;
         }
     }
 
     updatePosition() {
-        if (this.movingProgressRemaining > 0)  {
             const [property, change] = this.directionUpdate[this.direction]; 
             this[property] += change;
             this.movingProgressRemaining -= 1;   
-        }
     }
 
-    updateSprite(state)  {
+    updateSprite()  {
 
-        if (this.isPlayerControlled && this.movingProgressRemaining === 0 && !state.arrow) {
-            this.sprite.setAnimation("idle-"+this.direction);
+        if (this.movingProgressRemaining > 0) {
+            this.sprite.setAnimation("walk-" + this.direction);
             return;
         }
-
-        if (this.movingProgressRemaining > 0)  {
-            this.sprite.setAnimation("walk-"+this.direction);
-        }
+        this.sprite.setAnimation("idle-" + this.direction);
     }
 }
